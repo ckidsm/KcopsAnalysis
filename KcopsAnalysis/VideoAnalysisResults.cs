0c42a2712cddae1432c7ee18b8eb897a46fb4241
ckidsm <ckidsm@hotmail.com>
2023-09-22 11:44:57 +0900

    csv 읽기위한 클래스 작성

---
+++ KcopsAnalysis/VideoAnalysisResults.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Printing;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+
+namespace KcopsAnalysis
+{
+    internal class VideoAnalysisResults
+    {
+        [DisplayName("영상재생시간")]
+        public int FrameNumber { get; set; }
+        [DisplayName("영상재생시간")]
+        public double VideoNumber { get; set; }
+
+        //https://www.csharptutorial.net/csharp-file/csharp-read-csv-file/
+        public List<Collation> SetCsvFilePacer(string filename)
+        {
+            using (StreamReader SrtReader = new StreamReader(filename))
+            { 
+               
+                using (CsvReader csvRead = new CsvReader((IParser)SrtReader))
+                {
+                  return (List<Collation>)csvRead.GetRecords<VideoAnalysisResults>();
+                }
+               
+            }
+            
+        }
+
+    }
+}
